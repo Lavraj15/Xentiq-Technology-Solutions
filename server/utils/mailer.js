630c2f9638bb6c31ea1f89abbbb34edfff650b1c
Lav Raj <lavraj@bradtechnology.com>
2026-09-20 11:49:19 +0530

    fix gmail smtp configuration

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,12 +1,16 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-// Uses Gmail SMTP with an App Password (see README for setup steps).
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 module.exports = transporter;
